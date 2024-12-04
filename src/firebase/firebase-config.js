e4d8fa5e109d14201a7ea605c25bba57b4cd6a3f
nischalkotamraju <kotamrajunischal101@gmail.com>
2024-12-03 19:38:52 -0600

    env stuff

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCiaWhPqzZTG429BbeGFhTDVDixkPwADgM",
-  authDomain: "genlink-7f986.firebaseapp.com",
-  projectId: "genlink-7f986",
-  storageBucket: "genlink-7f986.firebasestorage.app",
-  messagingSenderId: "1084098783321",
-  appId: "1:1084098783321:web:776fe72dd0ed9a06292948",
-  measurementId: "G-YQ2EP9VJ4B"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
